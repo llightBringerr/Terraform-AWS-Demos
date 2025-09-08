5b13fbb5514f0ce753afe921d8b400818f6bc0c8
lightBringer <deepakgarg.aws@proton.me>
2025-09-08 14:20:59 +0000

    Declared t3.micro with  in lookup function

--- My First Instance in Workspaces (dev)/main.tf
+++ My First Instance in Workspaces (dev)/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "demo1" {
     ami           = var.ami_id # variable for AMI ID
-    instance_type = lookup(var.instance_type, terraform.workspace, t3.micro) # lookup instance type based on workspace, default to t3.micro
+    instance_type = lookup(var.instance_type, terraform.workspace, "t3.micro") # lookup instance type based on workspace, default to t3.micro
 
     tags = {
         Name = "DemoInstance1" # Tagging the instance with a name 
