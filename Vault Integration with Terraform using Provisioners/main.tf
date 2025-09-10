903a7eb63bd88bbcb5bcd8cc282b002b0d357706
lightBringer <deepakgarg.aws@proton.me>
2025-09-10 23:02:15 +0000

    Added various tags

--- Vault Integration with Terraform using Provisioners/main.tf
+++ Vault Integration with Terraform using Provisioners/main.tf
@@ -1,5 +1,9 @@
 resource "aws_vpc" "demo_vpc" {
     cidr_block = "10.0.0.0/16" # CIDR block for the VPC
+
+    tags = {
+        Name = "VaultVPC" # Tagging the VPC with a name
+    }
 }
 
 resource "aws_subnet" "demo_sub1" {
@@ -7,10 +11,18 @@ resource "aws_subnet" "demo_sub1" {
     cidr_block        = "10.0.0.0/24" # CIDR block for the subnet
     availability_zone = "ap-south-1a" # Availability zone for the subnet
     map_public_ip_on_launch = true # Enable public IP assignment on launch
+
+    tags = {
+        Name = "VaultSubnet" # Tagging the subnet with a name
+    }
 }   
 
 resource "aws_internet_gateway" "demo_igw" {
     vpc_id = aws_vpc.demo_vpc.id # Reference to the VPC created above
+
+    tags = {
+        Name = "VaultIGW" # Tagging the Internet Gateway with a name
+    }
 }
 
 resource "aws_route_table" "demo_rt" {
@@ -20,11 +32,16 @@ resource "aws_route_table" "demo_rt" {
         cidr_block = "0.0.0.0/0" # Route for all outbound traffic
         gateway_id = aws_internet_gateway.demo_igw.id # Reference to the Internet Gateway created above
     }
+
+    tags = {
+        Name = "VaultRouteTable" # Tagging the route table with a name
+    }
 }
 
 resource "aws_route_table_association" "demo_rta" {
     subnet_id      = aws_subnet.demo_sub1.id # Reference to the subnet created above
     route_table_id = aws_route_table.demo_rt.id # Reference to the route table created above
+
 }
 
 resource "aws_security_group" "demo_sg" {
@@ -37,7 +54,7 @@ resource "aws_security_group" "demo_sg" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"] # Open to all IPs for demonstration purposes
     }
-
+    
     ingress {
         from_port   = 8200 # Allow Vault access
         to_port     = 8200
@@ -51,9 +68,13 @@ resource "aws_security_group" "demo_sg" {
         protocol    = "-1" # -1 means all protocols
         cidr_blocks = ["0.0.0.0/0"] # Open to all IPs
     }
+
+    tags = {
+        Name = "VaultSecurityGroup" # Tagging the security group with a name
+    }
 }
 
-resource "aws_instance" "vaultinstance" {
+resource "aws_instance" "vault_instance" {
     ami           = var.ami_id_ubuntu # variable for ubuntu AMI ID
     instance_type = lookup(var.instance_type, terraform.workspace, "t3.micro") # lookup instance type based on workspace, default to t3.micro
     subnet_id     = aws_subnet.demo_sub1.id # Reference to the subnet created above
--- Vault Integration with Terraform using Provisioners/outputs.tf
+++ Vault Integration with Terraform using Provisioners/outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip_address" {
     description = "The public IP address of the instance"
-    value       = aws_instance.vaultinstance.public_ip
+    value       = aws_instance.vault_instance.public_ip
 }
