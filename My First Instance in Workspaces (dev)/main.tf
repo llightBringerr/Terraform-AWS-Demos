62ba66e2d7f7deb6e52fde1e662013c2d8b9b5e5
lightBringer <deepakgarg.aws@proton.me>
2025-09-10 23:14:06 +0000

    Deleted old duplicate projects

--- My First Instance in Workspaces (dev)/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "demo1" {
-    ami           = var.ami_id # variable for AMI ID
-    instance_type = lookup(var.instance_type, terraform.workspace, "t3.micro") # lookup instance type based on workspace, default to t3.micro
-
-    tags = {
-        Name = "DemoInstance1" # Tagging the instance with a name 
-    }
-}
--- My First Instance in Workspaces (dev)/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "public_ip_address" {
-  description = "The public IP address of the instance"
-  value       = aws_instance.demo1.public_ip
-  
-}
